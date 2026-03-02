0336367b7020b63f059ccff469ad70bb0e208cfd
Pragya Rajput <pragya.rajput2019@gmail.com>
2026-03-02 21:50:36 +0530

    Update removeFromArray.js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr , argnd) {
+
+  let NewArr;
+  
+  for(int i = 0; i < arr.size(); i++)
+  {
+    if(arr[i] == argnd)
+     NewArr = arr.filter( arr => arr[i] !== argnd );
+    
+  }
+  
 };
 
 // Do not edit below this line
