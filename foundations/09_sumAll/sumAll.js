2d7887dcc3aaf6dac5f2060c121b2c03f57124cc
Pragya Rajput <pragya.rajput2019@gmail.com>
2026-03-03 22:10:46 +0530

    sumAll.js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(int lowerVal , int HigherLimit) 
+{
+    int sum = 0;
+     for(int i = lowerVal; i <= HigherLimit; i++ )
+      {
+          sum = sum + i;                                               
+      }
+      return sum;
 };
 
 // Do not edit below this line
